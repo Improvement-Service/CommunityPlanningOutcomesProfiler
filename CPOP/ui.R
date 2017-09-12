2d65e45b9c4f6533fff7109709868cc12c1f5d1d
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2017-09-12 12:53:34 +0100

    Add UI to all tabs

--- CPOP/ui.R
+++ CPOP/ui.R
@@ -1,25 +1,57 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(navbarPage("CPOP",
  tabPanel("Cover Page/Contents",
           includeHTML("C:/Users/cassidy.nicholas/OneDrive - IS/CommunityPlanningOutcomesProfiler/CoverPage.html"),
           img(src = "http://www.improvementservice.org.uk/benchmarking/images/islogo.png", align = "top")),
- tabPanel("CPP - Page1"),
- tabPanel("CPP -Page2"),
+ tabPanel("CPP - Page1",
+          fluidPage(
+            fluidRow(
+              column(6,
+                     selectInput("LA1", "Select A Local Authority", c("Aberdeen City", "Some Other Council"))
+              ),
+              column(6,
+                     selectInput("CompLA1", "Comparator", c("Edinburgh", "Glasgow")
+                     )
+              )
+            ),
+            hr(),
+            fluidRow(
+              mainPanel(
+                p("PUT PLOT(S) HERE!")
+              )
+            )
+          )),
+ tabPanel("CPP -Page2",
+          fluidPage(
+            fluidRow(
+              column(6,
+                     selectInput("LA2", "Select A Local Authority", c("Aberdeen City", "Some Other Council"))
+              ),
+              column(6,
+                     selectInput("CompLA2", "Comparator", c("Edinburgh", "Glasgow")
+                     )
+              )
+            ),
+            hr(),
+            fluidRow(
+              mainPanel(
+                p("PUT PLOT(S) HERE!")
+              )
+            )
+          )),
  tabPanel("CPP - Page3",
   fluidPage(
     fluidRow(
       column(6,
-      selectInput("LA", "Select A Local Authority", c("Aberdeen City", "Some Other Council"))
+      selectInput("LA3", "Select A Local Authority", c("Aberdeen City", "Some Other Council"))
       ),
       column(6,
-             selectInput("CompLA", "Comparator", c("Edinburgh", "Glasgow")
+             selectInput("CompLA3", "Comparator", c("Edinburgh", "Glasgow")
               )
              )
     ),
     hr(),
-    # Show a plot of the generated distribution
     fluidRow(
     mainPanel(
        p("PUT PLOT(S) HERE!")
