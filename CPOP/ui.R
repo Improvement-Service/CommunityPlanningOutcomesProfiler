2cbb02853e1a94b6833a2b3eda3d0568021bf172
Nicholas Cassidy <nicholas.cassidy@improvementservice.org.uk>
2017-09-13 11:54:08 +0100

    Add link to PC, fix drop down lists

--- CPOP/ui.R
+++ CPOP/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 
 shinyUI(navbarPage("CPOP",
  tabPanel("Cover Page/Contents",
-          includeHTML("C:/Users/connachan.cara/Documents/CommunityPlanningOutcomesProfiler/CoverPage.html"),
+          includeHTML("C:/Users/nickm/Documents/CommunityPlanningOutcomesProfiler/CoverPage.html"),
           img(src = "http://www.improvementservice.org.uk/benchmarking/images/islogo.png", align = "top")),
  tabPanel("CPP - Page1",
           fluidPage(
@@ -27,10 +27,11 @@ shinyUI(navbarPage("CPOP",
           fluidPage(
             fluidRow(
               column(6,
-                     selectInput("LA2", "Select A Local Authority", c("Aberdeen City", "Some Other Council"))
+                     selectInput("LA2", "Select A Local Authority", unique(filter(CPPdta, CPP != "Scotland")[[1]]), 
+                                 selected = "Aberdeen City")
               ),
               column(6,
-                     selectInput("CompLA2", "Comparator", c("Edinburgh", "Glasgow")
+                     selectInput("CompLA2", "Select Comparator", unique(CPPdta$CPP), selected = "Scotland"
                      )
               )
             ),
@@ -45,10 +46,11 @@ shinyUI(navbarPage("CPOP",
   fluidPage(
     fluidRow(
       column(6,
-      selectInput("LA3", "Select A Local Authority", c("Aberdeen City", "Some Other Council"))
+      selectInput("LA3", "Select A Local Authority", unique(filter(CPPdta, CPP != "Scotland")[[1]]), 
+                  selected = "Aberdeen City")
       ),
       column(6,
-             selectInput("CompLA3", "Comparator", c("Edinburgh", "Glasgow")
+             selectInput("CompLA3", "Select Comparator", unique(CPPdta$CPP), selected = "Scotland"
               )
              )
     ),
