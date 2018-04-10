1752109db7fd7c66654cba7125081e1ea8f4d035
Cara Connachan <cara.connachan@improvementservice.org.uk>
2018-04-10 16:54:48 +0100

    Started adding CPPScore to IGZ Data

--- global.R
+++ global.R
@@ -14,3 +14,27 @@ CPPdta <- read_csv("CPPcleandata.csv")
 IGZdta <- read_csv("IGZcleandata.csv")
 DZdta <- read_csv("DZcleandata.csv")
 
+#Add column to IGZ data to specify whether a high value is positive
+IGZdta <- IGZdta %>% mutate(`High is Positive?` = "Yes")
+IGZdta$`High is Positive?`[IGZdta$Indicator %in% c("Child Poverty","Out of Work Benefits", "Crime Rate",
+                                                    "Emergency Admissions", "Early Mortality", "Depopulation")] <- "No"
+
+####Create CPP Scores and Type Scores for most recent years data
+#Filter data to most recent years data
+IGZ1617 <- filter(IGZdta, Year == "2016/17")
+
+#Group by Indicator and CPP and calculate CPP mean
+IGZ1617 <- ddply(IGZ1617,. (CPP, Indicator), transform, CPPMean = (mean(value)))
+
+#Calculate score minus group mean
+IGZ1617$Differences <- IGZ1617$value - IGZ1617$CPPMean
+
+#Square this difference
+IGZ1617$SquaredDiff <- IGZ1617$Differences*IGZ1617$Differences
+
+#Group by indicator and CPP and sum these squared differences 
+IGZ1617 <- ddply(IGZ1617,. (CPP, Indicator), transform, SumSquares = (sum(Differences)))
+
+#Group by CPP and count number of IGZ's within CPP
+IGZ1617 <- ddply(IGZ1617,. (CPP,Indicator), transform, N = (length(InterZone)))
+
