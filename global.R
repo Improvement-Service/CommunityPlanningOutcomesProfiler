e8ecaac4964ff1b483269b151f168d2caa2daa7f
Cara Connachan <cara.connachan@improvementservice.org.uk>
2018-04-11 15:36:09 +0100

    Completed CPP Score and Typology Score

--- global.R
+++ global.R
@@ -23,6 +23,8 @@ IGZdta$`High is Positive?`[IGZdta$Indicator %in% c("Child Poverty","Out of Work 
 #Filter data to most recent years data
 IGZ1617 <- filter(IGZdta, Year == "2016/17")
 
+
+##CPP Score
 #Group by Indicator and CPP and calculate CPP mean
 IGZ1617 <- ddply(IGZ1617,. (CPP, Indicator), transform, CPPMean = (mean(value)))
 
@@ -33,8 +35,55 @@ IGZ1617$Differences <- IGZ1617$value - IGZ1617$CPPMean
 IGZ1617$SquaredDiff <- IGZ1617$Differences*IGZ1617$Differences
 
 #Group by indicator and CPP and sum these squared differences 
-IGZ1617 <- ddply(IGZ1617,. (CPP, Indicator), transform, SumSquares = (sum(Differences)))
+IGZ1617 <- ddply(IGZ1617,. (CPP, Indicator), transform, SumSquares = (sum(SquaredDiff)))
 
 #Group by CPP and count number of IGZ's within CPP
 IGZ1617 <- ddply(IGZ1617,. (CPP,Indicator), transform, N = (length(InterZone)))
 
+#Calculate sums of squares divided by N -1 
+IGZ1617$Nminus1 <- IGZ1617$N - 1
+IGZ1617$Variance <- IGZ1617$SumSquares/IGZ1617$Nminus1
+
+#Calculate square root of variance
+IGZ1617$StandardDeviation <- sqrt(IGZ1617$Variance)
+
+#Calculate difference divided by standard deviation
+IGZ1617$ZScore <- IGZ1617$Differences/IGZ1617$StandardDeviation
+
+#If high is bad multiply Z score by minus 1 
+IGZ1617 <- filter(IGZ1617, High.is.Positive. == "No") %>%
+            mutate(CPPScore = ZScore * -1)
+IGZ1617 <- select(IGZ1617, c(-CPPMean, -Differences, -SquaredDiff, -SumSquares, -N, -Nminus1,
+                             -Variance, -StandardDeviation, -ZScore))
+
+##Type Score
+#Group by Indicator and Typology Group and calculate Type mean
+IGZ1617 <- ddply(IGZ1617,. (Typology_Group, Indicator), transform, TypeMean = (mean(value)))
+
+#Calculate score minus group mean
+IGZ1617$Differences <- IGZ1617$value - IGZ1617$TypeMean
+
+#Square this difference
+IGZ1617$SquaredDiff <- IGZ1617$Differences*IGZ1617$Differences
+
+#Group by indicator and Typology and sum these squared differences 
+IGZ1617 <- ddply(IGZ1617,. (Typology_Group, Indicator), transform, SumSquares = (sum(SquaredDiff)))
+
+#Group by Typology and count number of IGZ's within typology group
+IGZ1617 <- ddply(IGZ1617,. (Typology_Group,Indicator), transform, N = (length(InterZone)))
+
+#Calculate sums of squares divided by N -1 
+IGZ1617$Nminus1 <- IGZ1617$N - 1
+IGZ1617$Variance <- IGZ1617$SumSquares/IGZ1617$Nminus1
+
+#Calculate square root of variance
+IGZ1617$StandardDeviation <- sqrt(IGZ1617$Variance)
+
+#Calculate difference divided by standard deviation
+IGZ1617$ZScore <- IGZ1617$Differences/IGZ1617$StandardDeviation
+
+#If high is bad multiply Z score by minus 1 
+IGZ1617 <- filter(IGZ1617, High.is.Positive. == "No") %>%
+  mutate(TypeScore = ZScore * -1)
+IGZ1617 <- select(IGZ1617, c(-TypeMean, -Differences, -SquaredDiff, -SumSquares, -N, -Nminus1,
+                             -Variance, -StandardDeviation, -ZScore))
