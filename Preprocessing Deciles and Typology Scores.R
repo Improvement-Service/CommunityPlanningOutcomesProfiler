16c4e92d386d7e33d9905a0c4ab9db22cd4eef5d
Cara Connachan <cara.connachan@improvementservice.org.uk>
2018-06-21 15:51:16 +0100

    tidied code and removed any reference to 1617

--- Preprocessing Deciles and Typology Scores.R
+++ Preprocessing Deciles and Typology Scores.R
@@ -8,13 +8,16 @@ library(Unicode)
 library(leaflet)
 library(cowplot)
 
-SpPolysDF<-read_rds("Files for Maps/Shapes.rds")
+SpPolysDF <- read_rds("Files for Maps/Shapes.rds")
 SpPolysIZ <- read_rds("Files for Maps/IZshapes.rds")
 CPPdta <- read_csv("CPPcleandata.csv")
 IGZdta <- read_csv("IGZcleandata.csv")
 DZdta <- read_csv("DZcleandata.csv")
 
-#Calculate percentiles for colours
+
+#Calculate percentiles for map colours------------
+
+
 povDecs <- c()
 for(i in unique(SpPolysDF@data$council)){
   x <- ntile(SpPolysDF@data[SpPolysDF@data$council == i, 13], 7)
@@ -56,141 +59,203 @@ for(i in unique(SpPolysDF@data$council)){
 SpPolysDF@data <- cbind(SpPolysDF@data, povDecs, tariffDecs, posDecs,benDecs,crimeDecs, admisDecs)
 rm(i, x, povDecs, tariffDecs, posDecs,benDecs,crimeDecs, admisDecs)
 
-#Add column to IGZ data to specify whether a high value is positive
+
+# Create CPP Scores and Type Scores for most recent years data----------
+
+
 IGZdta <- IGZdta %>% mutate(`High is Positive?` = "Yes")
-IGZdta$`High is Positive?`[IGZdta$Indicator %in% c("Child Poverty","Out of Work Benefits", "Crime Rate",
-                                                   "Emergency Admissions", "Early Mortality", "Depopulation")] <- "No"
+IGZdta$`High is Positive?`[IGZdta$Indicator %in% c("Child Poverty","Out of Work Benefits", 
+                                                   "Crime Rate", "Emergency Admissions", 
+                                                   "Early Mortality", "Depopulation")] <- "No"
 
-####Create CPP Scores and Type Scores for most recent years data
-#Filter data to most recent years data
-IGZ1617 <- filter(IGZdta, Year == "2016/17")
+IGZ_latest <- filter(IGZdta, Year == "2016/17")
+
+# CPP Score
+
+IGZ_latest <- ddply(
+  IGZ_latest,. 
+  (CPP, Indicator), 
+  transform, 
+  CPPMean = (mean(value))
+  )
+
+IGZ_latest$Differences <- IGZ_latest$value - IGZ_latest$CPPMean
+
+IGZ_latest <- ddply(
+  IGZ_latest,. 
+  (CPP, Indicator), 
+  transform, 
+  StdDev = (sd(value))
+  )
+
+IGZ_latest$ZScore <- IGZ_latest$Differences / IGZ_latest$StdDev
+
+# If high is bad multiply Z score by minus 1 to ensure direction is the same for all indicators
+
+IGZ_latest$CPPScore <- IGZ_latest$ZScore
+
+IGZ_latest$CPPScore[IGZ_latest$High.is.Positive. == "No"] <- 
+  (IGZ_latest$CPPScore[IGZ_latest$High.is.Positive. =="No"]) * -1
+
+IGZ_latest <- select(IGZ_latest, c(-CPPMean, -Differences, -StdDev, -ZScore))
+
+# Type Score
+
+IGZ_latest <- ddply(
+  IGZ_latest,. 
+  (Typology_Group, Indicator), 
+  transform, 
+  TypeMean = (mean(value))
+  )
+
+IGZ_latest$Differences <- IGZ_latest$value - IGZ_latest$TypeMean
+
+IGZ_latest <- ddply(
+  IGZ_latest,. 
+  (Typology_Group, Indicator), 
+  transform, 
+  StdDev = (sd(value))
+  )
+
+IGZ_latest$ZScore <- IGZ_latest$Differences / IGZ_latest$StdDev
+
+# If high is bad multiply Z score by minus 1 to ensure direction is the same for all indicators
+
+IGZ_latest$TypeScore <- IGZ_latest$ZScore
+
+IGZ_latest$TypeScore[IGZ_latest$High.is.Positive. =="No"] <-
+  (IGZ_latest$TypeScore[IGZ_latest$High.is.Positive. =="No"]) * -1
+
+IGZ_latest <- select(IGZ_latest, c(-TypeMean, -Differences, -StdDev, -ZScore))
 
 
-##CPP Score
-#Group by Indicator and CPP and calculate CPP mean
-IGZ1617 <- ddply(IGZ1617,. (CPP, Indicator), transform, CPPMean = (mean(value)))
+# Create CPP Scores & Typology Scores for the change from start to finish year--------------
 
-#Calculate score minus group mean
-IGZ1617$Differences <- IGZ1617$value - IGZ1617$CPPMean
+# calculate overall zscore for change
 
-#Group by CPP and Indicator and calculate Standard Deviation
-IGZ1617 <- ddply(IGZ1617,. (CPP, Indicator), transform, StandardDeviation = (sd(value)))
+IGZ_change <- filter(IGZdta, Year %in% c("2006/07","2016/17"))
 
-#Calculate difference divided by standard deviation
-IGZ1617$ZScore <- IGZ1617$Differences/IGZ1617$StandardDeviation
+# need to group by CPP + IGZ here to stop IGZ's with the same name getting included in the wrong group
 
-#If high is bad multiply Z score by minus 1
-IGZ1617$CPPScore <- IGZ1617$ZScore
-IGZ1617$CPPScore[IGZ1617$High.is.Positive. =="No"] <- (IGZ1617$CPPScore[IGZ1617$High.is.Positive. =="No"])*-1
-IGZ1617 <- select(IGZ1617, c(-CPPMean, -Differences, -StandardDeviation, -ZScore))
+IGZ_change <- ddply(
+  IGZ_change,. 
+  (InterZone, CPP, Indicator), 
+  transform, 
+  Change = (last(value) / first(value) -1)
+  )
 
-##Type Score
-#Group by Indicator and Typology Group and calculate Type mean
-IGZ1617 <- ddply(IGZ1617,. (Typology_Group, Indicator), transform, TypeMean = (mean(value)))
+# If high is bad multiply Z score by minus 1 to ensure direction is the same for all indicators
 
-#Calculate score minus group mean
-IGZ1617$Differences <- IGZ1617$value - IGZ1617$TypeMean
+IGZ_change$Change[IGZ_change$High.is.Positive. == "No"] <- 
+  (IGZ_change$Change[IGZ_change$High.is.Positive. == "No"]) * -1
 
-#Group by Typology Group and Indicator and calculate Standard Deviation
-IGZ1617 <- ddply(IGZ1617,. (Typology_Group, Indicator), transform, StandardDeviation = (sd(value)))
+# Filtering data so that change value is only included once per IGZ
 
-#Calculate difference divided by standard deviation
-IGZ1617$ZScore <- IGZ1617$Differences/IGZ1617$StandardDeviation
+IGZ_change <- filter(IGZ_change, Year == "2016/17")
 
-#If high is bad multiply Z score by minus 1
-IGZ1617$TypeScore <- IGZ1617$ZScore
-IGZ1617$TypeScore[IGZ1617$High.is.Positive. =="No"] <- (IGZ1617$TypeScore[IGZ1617$High.is.Positive. =="No"])*-1
-IGZ1617 <- select(IGZ1617, c(-TypeMean, -Differences, -StandardDeviation, -ZScore))
+IGZ_change <- ddply(
+  IGZ_change,. 
+  (Indicator), 
+  transform, 
+  OverallMean = (mean(Change))
+  )
 
-####Create CPP Change Scores and Typology Scores for the change from start to finish year
+IGZ_change$Differences <- IGZ_change$Change - IGZ_change$OverallMean
 
-#filter data to start and finish year
-IGZChange <- filter(IGZdta, Year %in% c("2006/07","2016/17"))
+IGZ_change <- ddply(
+  IGZ_change,. 
+  (Indicator), 
+  transform, 
+  StdDev = (sd(Change))
+  )
 
-#Group data by IGZ and Indicator and calculate change from start year to finish year
-IGZChange <- ddply(IGZChange,. (InterZone, Indicator), transform, Change = (last(value)/first(value)-1))
+IGZ_change$OverallZScore <- IGZ_change$Differences / IGZ_change$StdDev
+IGZ_change <- select(IGZ_change, c(-OverallMean, -Differences, -StdDev))
 
-#If High is bad multiply change value by minus 1
-IGZChange$Change[IGZChange$High.is.Positive. == "No"] <- (IGZChange$Change[IGZChange$High.is.Positive. == "No"])*-1
+# Calculate CPP Change Score
 
-##Calculate overall Z Score for change value
+IGZ_change <- ddply(
+  IGZ_change,. 
+  (CPP, Indicator), 
+  transform, 
+  CPPMean = (mean(OverallZScore))
+  )
 
-#Filter data so that change value is only included once per IGZ
-IGZChange <- filter(IGZChange, Year == "2016/17")
+IGZ_change$Differences <- IGZ_change$OverallZScore - IGZ_change$CPPMean
 
-#Group by indicator and calculate mean of change value
-IGZChange <- ddply(IGZChange,. (Indicator), transform, OverallMean = (mean(Change)))
+IGZ_change <- ddply(
+  IGZ_change,. 
+  (CPP, Indicator), 
+  transform, 
+  StdDev = (sd(OverallZScore))
+  )
 
-#Calculate change score minus overall mean
-IGZChange$Differences <- IGZChange$Change - IGZChange$OverallMean
+IGZ_change$CPPChangeScore <- IGZ_change$Differences / IGZ_change$StdDev
+IGZ_change <- select(IGZ_change, c(-CPPMean, -Differences, -StdDev))
 
-#Group by indicator and calculate Standard Deviation
-IGZChange <- ddply(IGZChange,. (Indicator), transform, StandardDeviation = (sd(Change)))
+# Calculate Typology Change Score
 
-#Calculate difference divided by Standard Deviation
-IGZChange$OverallZScore <- IGZChange$Differences/IGZChange$StandardDeviation
-IGZChange <- select(IGZChange, c(-OverallMean, -Differences, -StandardDeviation))
+IGZ_change <- ddply(
+  IGZ_change,. 
+  (Typology_Group, Indicator), 
+  transform, 
+  TypeMean = (mean(OverallZScore))
+  )
 
-##Calculate CPP Change Score
+IGZ_change$Differences <- IGZ_change$OverallZScore - IGZ_change$TypeMean
 
-#Group by Indicator and CPP and calculate mean of overall Z score
-IGZChange <- ddply(IGZChange,. (CPP, Indicator), transform, CPPMean = (mean(OverallZScore)))
+IGZ_change <- ddply(
+  IGZ_change,. 
+  (Typology_Group, Indicator), 
+  transform, 
+  StdDev = (sd(OverallZScore))
+  )
 
-#Calculate overallZscore minus CPPmean
-IGZChange$Differences <- IGZChange$OverallZScore - IGZChange$CPPMean
+IGZ_change$TypeChangeScore <- IGZ_change$Differences / IGZ_change$StdDev
+IGZ_change <- select(IGZ_change, c(-TypeMean, -Differences, -StdDev))
 
-#Group by Indicator and CPP and calculate Standard Deviation
-IGZChange <- ddply(IGZChange,. (CPP, Indicator), transform, StandardDeviation = (sd(OverallZScore)))
 
-#Calculate differences divided by Standard Deviation
-IGZChange$CPPChangeScore <- IGZChange$Differences/IGZChange$StandardDeviation
-IGZChange <- select(IGZChange, c(-CPPMean, -Differences, -StandardDeviation))
+# Add Z score column to SpDF to allow ranking in this DataFrame --------------------- 
 
-##Calculate Typology Change Score
 
-#Group by Indicator and Typology group and calculate mean of overall Z score
-IGZChange <- ddply(IGZChange,. (Typology_Group, Indicator), transform, TypeMean = (mean(OverallZScore)))
-
-#Calculate overallZscore minus Typemean
-IGZChange$Differences <- IGZChange$OverallZScore - IGZChange$TypeMean
-
-#Group by Indicator and Typology Group and calculate Standard Deviation
-IGZChange <- ddply(IGZChange,. (Typology_Group, Indicator), transform, StandardDeviation = (sd(OverallZScore)))
-
-#Calculate differences divided by Standard Deviation
-IGZChange$TypeChangeScore <- IGZChange$Differences/IGZChange$StandardDeviation
-IGZChange <- select(IGZChange, c(-TypeMean, -Differences, -StandardDeviation))
-
-##For IGZs add Z score column to SpDF
 decs <- c()
-decs <- ddply(IGZ1617, .(InterZone, CPP), summarise, combCPP = sum(CPPScore)) %>%
+
+# need to group by CPP + IGZ here to stop IGZ's with the same name getting included in the wrong group
+
+decs <- ddply(IGZ_latest,.(InterZone, CPP), summarise, combCPP = sum(CPPScore)) %>%
   ddply(., .(CPP), mutate, CPPDec = ntile(combCPP, n = 7)) %>% 
-  ddply(.,.(CPP), mutate, CPPRank = frank(combCPP)) %>% select(InterZone, CPPDec,CPPRank)
+  ddply(.,.(CPP), mutate, CPPRank = frank(combCPP)) %>% 
+  select(InterZone, CPPDec,CPPRank)
+
 SpPolysIZ@data <- left_join(SpPolysIZ@data, decs, by = "InterZone") %>% select(-rank_decs, -`rank-min`)
 names(SpPolysIZ@data)[c(13,14)] <- c("rank_decs", "rank-min")
 
 saveRDS(SpPolysDF, "Files for Maps/Shapes_decs.rds")
 saveRDS(SpPolysIZ, "Files for Maps/IZshapes_decs.rds")
-write_csv(IGZChange,"IGZChangeTypology.csv")
-write_csv(IGZ1617,"IGZ1617Typology.csv")
+write_csv(IGZ_change,"IGZ_change.csv")
+write_csv(IGZ_latest,"IGZ_latest.csv")
 
 
-##########
-#Compute improvement rates and store data for plots on page 1
+# Compute CPP improvement rates and store data for plots on CPP over time page--------
 
-#add new column to data so that line type can be specified
-CPPdtaCurrent <- CPPdta %>% mutate(Grouping=  paste(CPP, Type))  
 
-#add new column to show rate of improvement 
-CPPdtaCurrent <- filter(CPPdtaCurrent, Type != "Projected")
-#CPPdtaCurrent <- ddply(CPPdtaCurrent, .(CPP, Indicator), transform, Improvement_Rate = ((last(value)/first(value)-1)*100))
-CPPdtaCurrent <- setDT(CPPdtaCurrent)[, Improvement_Rate :=(last(value)/first(value)-1)*100,by = list(CPP, Indicator)]
-#add new column to show whether a high value represents a positive outcome
-CPPdtaCurrent <- CPPdtaCurrent %>% mutate(`High is Positive?` = "Yes")
-CPPdtaCurrent$`High is Positive?`[CPPdtaCurrent$Indicator %in% c("Dwelling Fires", "Unplanned Hospital Attendances",
-                                                                 "Fuel Poverty", "Fragility", "Carbon Emissions",
-                                                                 "Child Poverty", "Out Of Work Benefits",
-                                                                 "Crime Rate", "Emergency Admissions",
-                                                                 "Early Mortality")] <- "No"
+CPP_dta_current <- CPPdta %>% 
+  mutate(LineType =  paste(CPP, Type)) %>%
+  filter(Type != "Projected")
 
-write_csv(CPPdtaCurrent, "ImpRateCPP.csv")
+CPP_dta_current <- setDT(CPP_dta_current)[, Improvement_Rate :=
+                                            (last(value) / first(value) -1) * 100,
+                                          by = list(CPP, Indicator)
+                                          ]
+
+CPP_dta_current <- CPP_dta_current %>% mutate(`High is Positive?` = "Yes")
+CPP_dta_current$`High is Positive?`[CPP_dta_current$Indicator %in% c("Dwelling Fires", 
+                                                                     "Unplanned Hospital Attendances",
+                                                                     "Fuel Poverty", "Fragility", 
+                                                                     "Carbon Emissions", "Child Poverty",
+                                                                     "Out Of Work Benefits", "Crime Rate",
+                                                                     "Emergency Admissions",
+                                                                     "Early Mortality")
+                                    ] <- "No"
+
+write_csv(CPP_dta_current, "Imp_rate_CPP.csv")
